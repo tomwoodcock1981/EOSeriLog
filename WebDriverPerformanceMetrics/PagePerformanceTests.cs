aaeed97d64510258419118308b22083a62b9ce9e
tomwoodcock1981 <woodcock40@gmail.com>
2023-11-02 16:04:32 +0000

    Update PagePerformanceTests.cs

--- WebDriverPerformanceMetrics/PagePerformanceTests.cs
+++ WebDriverPerformanceMetrics/PagePerformanceTests.cs
@@ -31,7 +31,7 @@ namespace Web_driverPerformanceMetrics
         public void LoginAndHomePage()
         {
             // Navigate to the website you want to measure
-            _driver.Navigate().GoToUrl("https://autoa.qa.energyone.com/enTrader/1120/");
+            _driver.Navigate().GoToUrl("ENVURL");
 
             _driver.PageLoad(_driver).FindElement(By.Id("ctl00_ContentBody_Login1_UserName")).SendKeys("Automation");
             _driver.FindElement(By.Id("ctl00_ContentBody_Login1_Password")).SendKeys("Password1");
